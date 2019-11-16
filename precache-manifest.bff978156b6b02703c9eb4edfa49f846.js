4ff16fb20f16bcac0f5d665b68cdf0c743c47dc2
gleb <gleb573802wow@mail.ru>
2019-11-16 22:40:06 +0300

    Updates

---
+++ precache-manifest.bff978156b6b02703c9eb4edfa49f846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb60a3620deff466e94a8a8e1edb506",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d9b30df8c1e658b365bd",
+    "url": "/react-gh-pages/static/css/main.52623433.chunk.css"
+  },
+  {
+    "revision": "43c793fbe2442a978b09",
+    "url": "/react-gh-pages/static/js/2.dccee69b.chunk.js"
+  },
+  {
+    "revision": "d9b30df8c1e658b365bd",
+    "url": "/react-gh-pages/static/js/main.e6f81ab0.chunk.js"
+  },
+  {
+    "revision": "b450ca89d840e7052f47",
+    "url": "/react-gh-pages/static/js/runtime-main.0a9a42a9.js"
+  }
+]);
