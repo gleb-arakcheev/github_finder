ba0cabb41e0444193cb08eaf2a77c2c4147fa9ac
gleb <gleb573802wow@mail.ru>
2019-11-17 17:56:07 +0300

    build folder added

---
+++ build/precache-manifest.02d416748b42673126e9e8dee50ddafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70248dd928305fefcd6a16b6aadeabec",
+    "url": "/github_finder/index.html"
+  },
+  {
+    "revision": "d9b30df8c1e658b365bd",
+    "url": "/github_finder/static/css/main.52623433.chunk.css"
+  },
+  {
+    "revision": "43c793fbe2442a978b09",
+    "url": "/github_finder/static/js/2.dccee69b.chunk.js"
+  },
+  {
+    "revision": "d9b30df8c1e658b365bd",
+    "url": "/github_finder/static/js/main.e6f81ab0.chunk.js"
+  },
+  {
+    "revision": "31000e3d80595b618ae1",
+    "url": "/github_finder/static/js/runtime-main.999ece4e.js"
+  }
+]);
